0b8633d21da46d2deb3845d2ce2b588196f6c910
Serhii Pytliak <pytliak.serhii@gmail.com>
2022-07-29 12:12:44 +0300

    added bucket - sandbox

--- terraform/project_percona/backend.tf
+++ terraform/project_percona/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-project-genesis-dev"
+    bucket = "tfstate-project-genesis-percona"
     key    = "genesis/percona.tfstate"
     region = "us-east-1"
   }
